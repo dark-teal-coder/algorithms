ac2475c50d839b8b76985e0da89d396d1f4c3e2e
Preesa Saewong <ree.wong.career@gmail.com>
2022-10-23 14:34:22 -0400

    Add python bubble sort algo

---
+++ bubble-sort.py
@@ -0,0 +1,29 @@
+def bubble_sort(items):
+    n = len(items)
+    for i in range(n):
+        # Create a flag that will allow the function to
+        # terminate early if there's nothing left to sort
+        already_sorted = True
+
+        # Start looking at each item of the list one by one,
+        # comparing it with its adjacent value. With each
+        # iteration, the portion of the array that you look at
+        # shrinks because the remaining items have already been
+        # sorted.
+        for j in range(n - i - 1):
+            if items[j] > items[j + 1]:
+                # If the item you're looking at is greater than its
+                # adjacent value, then swap them
+                items[j], items[j + 1] = items[j + 1], items[j]
+
+                # Since you had to swap two elements,
+                # set the `already_sorted` flag to `False` so the
+                # algorithm doesn't finish prematurely
+                already_sorted = False
+
+        # If there were no swaps during the last iteration,
+        # the array is already sorted, and you can terminate
+        if already_sorted:
+            break
+
+    return items
